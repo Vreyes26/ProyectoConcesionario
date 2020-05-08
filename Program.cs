6b3f35e4000292f9d9c4326e77fea8483508eebb
Vreyes26 <Vreyes26@users.noreply.github.com>
2020-05-07 23:12:43 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ProyectoConcesionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user;
+            string password;
+            int menu = 0;
+
+            do
+            {
+                Console.WriteLine("Usuario: ");
+                user = Console.ReadLine();
+                Console.WriteLine("Contraseña: ");
+                password = Console.ReadLine();
+
+                if (user != "admin" && password != "abc123")
+                {
+                    Console.WriteLine("Error, usuario o contraseña incorrecto");
+                }
+            } while (user != "admin" && password != "abc123");
+
+            do
+            {
+                Console.WriteLine("Ingrese: \n" +
+                    "1: Agregar vehiculo \n" +
+                    "2: Ver datos vehiculo\n" +
+                    "0: Salir");
+                menu = int.Parse(Console.ReadLine());
+
+            } while (menu != 0);
+        }
+    }
+}
